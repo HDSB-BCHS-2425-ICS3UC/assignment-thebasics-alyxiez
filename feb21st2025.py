4caeb47dada3262781f444940739f18dbf51b314
alyxiez <1ziubinskale@hdsb.ca>
2025-02-21 14:19:44 -0500

    create the file

---
+++ feb21st2025.py
@@ -0,0 +1,10 @@
+'''
+author: alex ziubinska
+date modified: february 21st, 2025
+description: a program that calculates the volume of a cube with the area
+'''
+
+print("Please enter the area of the cube.") # ask the user to input the area
+area_input = float(input()) # actually prompt them to input it
+
+print("The volume of the cube is " + str(area_input**3) + ".") # output the result
