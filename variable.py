4dc5f7e2e9ed64577a8a530cef82a48843e25201
alyxiez <1ziubinskale@hdsb.ca>
2025-02-26 13:49:58 -0500

    just gonna commit this rq

---
+++ variable.py
@@ -0,0 +1,14 @@
+1 == 39 
+# this is an integer
+2 == 80.25 
+# this is a float
+3 == 0.23904712339846128123124 
+# this is a double
+4 == "h" 
+# this is a char
+5 == True 
+# this is a boolean
+6 == "demonstration!" 
+# this is a string
+
+print("1 is a integer.\n2 is a float.\n3 is a double.\n4 is a char.\n5 is a boolean.\n6 is a string.")
